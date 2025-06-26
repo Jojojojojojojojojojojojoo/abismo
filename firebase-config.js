3acb5ef5c651b4ea6ce133fb05a1ce8f46dde133
Kurb koor <98238244+Jojojojojojojojojojojojoo@users.noreply.github.com>
2025-06-26 05:36:26 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,9 @@
-
+// Substitua com os dados do seu projeto Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDkXd3yrkTLLZTCHSOQh0tK5V8IqoEX4fA",
+  authDomain: "mansion-f8db0.firebaseapp.com",
+  projectId: "mansion-f8db0",
+  storageBucket: "mansion-f8db0.firebasestorage.app",
+  messagingSenderId: "59483603849",
+  appId: "1:59483603849:web:b80ab876f41e7262e75758"
+};
